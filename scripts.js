755764fbf3a970f56f565bb47eb1c02fb23f1668
Lindsteez <lindgren.axel@hotmail.com>
2025-11-10 08:56:05 +0100

    added weatherRes and correct timezone

--- scripts.js
+++ scripts.js
@@ -97,11 +97,16 @@ async function getWeatherByCity(cityName) {
     const geoCor = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${cityName}&count=10&language=en&format=json`);
     const geoData = await geoCor.json();
 
-    const swedishResult = geoData.results.find(r => r.country_code === "SE");
-    console.log("Swedish Geocoding Result:", swedishResult);
-    return swedishResult;
+      const swedishResult = geoData.results.find(r => r.country_code === "SE");
+      const { latitude, longitude } = swedishResult;
+      console.log("Swedish Geocoding Result:", swedishResult);
+
+      const weatherRes = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&timezone=Europe/Stockholm`);
+      const weatherData = await weatherRes.json();
+      console.log("Current Weather Data:", weatherData);
+      return swedishResult;
   }
-  
+
   catch (error) {
     console.error("Error fetching weather data:", error);
   }
